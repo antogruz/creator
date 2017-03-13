45272d14057579827ea8094890e464beaf922d06
Antoine Gruzelle <antogruz@gmail.com>
2017-03-13 15:56:29 +0100

    generate_grids.py calls generate_grid_css & generate_grid_html

--- scripts/generate_grid_css.py
+++ scripts/generate_grid_css.py
@@ -1,27 +1,6 @@
 import argparse
 
 from css_class import CssClass
-from generate_grid_html import generate_grid_html
-
-height = 350.0
-width = 227.0
-cssfile = "view/css/grid.css"
-htmlfile = "view/grid.html"
-
-def main():
-    columns = get_columns_count()
-    lines = int(height * columns / width) + 1
-    grid_css = generate_grid_css(lines, columns)
-    create_file(cssfile, grid_css)
-    grid_html = '<div class="marge carte-size">'
-    grid_html += generate_grid_html(lines, columns)
-    grid_html += '</div>'
-    create_file(htmlfile, grid_html)
-
-def get_columns_count():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("columns", type=int)
-    return parser.parse_args().columns
 
 def generate_grid_css(lines, columns):
     return generate_all_lines(lines)
@@ -73,8 +52,3 @@ def generate_all(count, generator):
 def get_percentage(dividende, divisor):
     return dividende * 100.0 / divisor
 
-def create_file(filename, content):
-    with open(filename, "w") as fh:
-        fh.write(content)
-
-main()
