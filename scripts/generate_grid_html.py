ae0e70d9bb3b521cc45bb7988cc66bd681f3f201
Antoine Gruzelle <antogruz@gmail.com>
2017-03-13 16:07:09 +0100

    Change grid html displayed numbers

--- scripts/generate_grid_html.py
+++ scripts/generate_grid_html.py
@@ -2,6 +2,6 @@ def generate_grid_html(lines, columns):
     html = ""
     for line in range(lines):
         for column in range(columns):
-            html += '<div class="line-{} col-{} w-1 h-1">{}-{}</div>'.format(line, column, line, column)
+            html += '<div class="line-{} col-{} w-1 h-1">{}</div>'.format(line, column, line * columns + column)
 
     return html
--- tests/grid_html.py
+++ tests/grid_html.py
@@ -8,15 +8,15 @@ class GridGeneratorTester(Tester):
         assert generate_grid_html(0, 1) == ""
 
     def one_quad_test(self):
-        assert generate_grid_html(1, 1) == '<div class="line-0 col-0 w-1 h-1">0-0</div>'
+        assert generate_grid_html(1, 1) == '<div class="line-0 col-0 w-1 h-1">0</div>'
 
     def two_lines_test(self):
         html = generate_grid_html(2, 1)
-        assert html == '<div class="line-0 col-0 w-1 h-1">0-0</div><div class="line-1 col-0 w-1 h-1">1-0</div>'
+        assert html == '<div class="line-0 col-0 w-1 h-1">0</div><div class="line-1 col-0 w-1 h-1">1</div>'
 
     def two_columns_test(self):
         html = generate_grid_html(1, 2)
-        assert html == '<div class="line-0 col-0 w-1 h-1">0-0</div><div class="line-0 col-1 w-1 h-1">0-1</div>'
+        assert html == '<div class="line-0 col-0 w-1 h-1">0</div><div class="line-0 col-1 w-1 h-1">1</div>'
 
 
 def main():
