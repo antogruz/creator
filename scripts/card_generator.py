7d16113a25c63cae7a18b6baa41d7de9479336f8
Antoine Gruzelle <antogruz@gmail.com>
2017-03-21 17:41:25 +0100

    split card generation into functions

--- scripts/card_generator.py
+++ scripts/card_generator.py
@@ -12,6 +12,16 @@ def wrap_in_card_container(content):
 def get_card_content():
     return """
 <div class="carte bleue">
+    {}
+    {}
+    {}
+    {}
+    {}
+    {}
+</div>""".format(get_cost(), get_dependency(), get_effect(), get_name(), get_picture(), get_players())
+
+def get_cost():
+    return """
     <div style="position:absolute;top:0px;left:10px;width:13px;height:82px">
         <div class="background-banner background-full"> </div>
     </div>
@@ -24,27 +34,47 @@ def get_card_content():
     <div style="position:absolute;top:53px;left:5px;width:24px;height:24px">
         <img class="full-screen" src="images/pierre.png"/>
     </div>
+"""
+
+def get_dependency():
+    return """
     <div style="position:absolute;top:0px;left:32px;width:12px;height:25px">
         <div class="background-banner background-full text-dependance">
         BAINS
         </div>
     </div>
+"""
+
+def get_effect():
+    return """
     <div style="position:absolute;top:10px;left:90px;width:60px;height:55px">
         <img class="full-screen" src="images/laurier3.png">
         <div class="center victoire chiffres">5</div>
     </div>
+"""
+
+def get_name():
+    return """
     <div style="position:absolute;bottom:0;left:10px;width:20px;height:90px">
         <div class="background-name background-full text-name">
             AQUEDUC
         </div>
     </div>
+    """
+
+def get_picture():
+    return """
     <div style="position:absolute;bottom:0;right:0;height:77%;width:82%">
         <img class="full-screen" src="images/aqueduc.png"/>
     </div>
+    """
+
+def get_players():
+    return """
     <div style="position:absolute;bottom:3px;left:50%" class="text-nombreJoueurs">
         3+
     </div>
-</div>"""
+    """
 
 import re
 def format(html):
