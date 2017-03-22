91822ebb8190354760cee6d95cedbf0d30249501
Antoine Gruzelle <antogruz@gmail.com>
2017-03-22 17:06:40 +0100

    Add tavern test

--- scripts/card_generator.py
+++ scripts/card_generator.py
@@ -1,45 +1,79 @@
-def generate_card():
-    return format(wrap_in_card_container(get_card_content()))
+class Config():
+    def __init__(self):
+        self.color = "bleue"
+        self.cost = ["pierre", "pierre", "pierre"]
+        self.dependency = "BAINS"
+        self.name = "AQUEDUC"
+        self.picture = "aqueduc.png"
+        self.players = 3
+        self.effect = {"victory": 5}
+
+
+def generate_card(config):
+    return format(wrap_in_card_container(get_card_content(config)))
 
 def wrap_in_card_container(content):
     return wrap_in_div('<div class="marge carte-size">', content)
 
-def get_card_content():
-    return wrap_in_div('<div class="carte bleue">', get_cost() + get_dependency() + get_effect() + get_name() + get_picture() + get_players())
+def get_card_content(config):
+    return wrap_in_div('<div class="carte {}">'.format(config.color), get_cost(config.cost) + get_dependency(config.dependency) + get_effect(config.effect) + get_name(config.name) + get_picture(config.picture) + get_players(config.players))
 
-def get_cost():
-    pierre = '<img class="full-screen" src="images/pierre.png"/>'
-    cost = ""
-    cost += wrap_in_div(position(0, 10, 13, 82), '<div class="background-banner background-full"> </div>')
+def get_cost(cost):
+    if not cost or len(cost) == 0:
+        return ""
+
+    html = wrap_in_div(position(0, 10, 13, 82), '<div class="background-banner background-full"> </div>')
     left = 5
     size = 24
-    cost += wrap_in_div(position(3, left, size, size), pierre)
-    cost += wrap_in_div(position(28, left, size, size), pierre)
-    cost += wrap_in_div(position(53, left, size, size), pierre)
-    return cost
+    top = 3
+    for resource_name in cost:
+        resource = '<img class="full-screen" src="images/{}.png"/>'.format(resource_name)
+        html += wrap_in_div(position(top, left, size, size), resource)
+        top += 25
+    return html
 
-def get_dependency():
-    return wrap_in_div(position(0, 32, 12, 25), wrap_in_div('<div class="background-banner background-full text-dependance">', "BAINS"))
+def get_dependency(name):
+    if not name or len(name) == 0:
+        return ""
 
-def get_effect():
-    victory_points = '<img class="full-screen" src="images/laurier3.png">' + wrap_in_div('<div class="center victoire chiffres">', "5")
-    return wrap_in_div(position(10, 90, 60, 55), victory_points)
+    return wrap_in_div(position(0, 32, 12, 25), wrap_in_div('<div class="background-banner background-full text-dependance">', name))
 
-def get_name():
-    name = wrap_in_div('<div class="background-name background-full text-name">', "AQUEDUC")
+def get_effect(effects):
+    html = ""
+    for key, value in effects.items():
+        html += create_effect(key, value)
+    return html
+
+def create_effect(label, value):
+    if label == "victory":
+        return victory(value)
+    if label == "coin":
+        return coin(value)
+    return ""
+
+def victory(n):
+    content = '<img class="full-screen" src="images/laurier3.png">' + wrap_in_div('<div class="center victoire chiffres">', str(n))
+    return wrap_in_div(position(10, 90, 60, 55), content)
+
+def coin(n):
+    content = '<img class="full-screen" src="images/coin.png">' + wrap_in_div('<div class="center coins chiffres">', str(n))
+    return wrap_in_div(position(10, 90, 55, 55), content)
+
+def get_name(card_name):
+    name = wrap_in_div('<div class="background-name background-full text-name">', card_name)
     return wrap_in_div('<div style="position:absolute;bottom:0;left:10px;width:20px;height:90px">', name)
 
-def get_picture():
-    return wrap_in_div('<div style="position:absolute;bottom:0;right:0;height:77%;width:82%">', '<img class="full-screen" src="images/aqueduc.png"/>')
+def get_picture(file_name):
+    return wrap_in_div('<div style="position:absolute;bottom:0;right:0;height:77%;width:82%">', '<img class="full-screen" src="images/{}"/>'.format(file_name))
 
-def get_players():
-    return wrap_in_div('<div style="position:absolute;bottom:3px;left:50%" class="text-nombreJoueurs">', "3+")
+def get_players(n):
+    return wrap_in_div(div([style(["position:absolute", dim("bottom", 3), dim("left", 50, "%")]), 'class="text-nombreJoueurs"']), "{}+".format(str(n)))
 
 def wrap_in_div(div, content):
     return div + content + "</div>"
 
 def position(top, left, width, height):
-    return div([style(["position:absolute", dimension("top", top), dimension("left", left), dimension("width", width), dimension("height", height)])])
+    return div([style(["position:absolute", dim("top", top), dim("left", left), dim("width", width), dim("height", height)])])
 
 def div(attributes):
     return "<div " + " ".join(attributes) + ">"
@@ -47,8 +81,8 @@ def div(attributes):
 def style(elements):
     return 'style="' + ";".join(elements) + '"'
 
-def dimension(label, value):
-    return "{}:{}px".format(label, value)
+def dim(label, value, unit="px"):
+    return "{}:{}{}".format(label, value, unit)
 
 import re
 def format(html):
--- tests/html_generation.py
+++ tests/html_generation.py
@@ -1,5 +1,5 @@
 from unittests import Tester
-from card_generator import generate_card, format
+from card_generator import generate_card, Config
 
 class CardsGeneratorTester(Tester):
     def aqueduc_test(self):
@@ -43,10 +43,48 @@ class CardsGeneratorTester(Tester):
     </div>
 </div>
 """
-        actual = generate_card()
+        actual = generate_card(Config())
         if not expected == actual:
             print(actual)
             assert False
+
+    def taverne_test(self):
+        expected = """<div class="marge carte-size">
+    <div class="carte jaune">
+        <div style="position:absolute;top:10px;left:90px;width:55px;height:55px">
+            <img class="full-screen" src="images/coin.png">
+            <div class="center coins chiffres">
+                5
+            </div>
+        </div>
+        <div style="position:absolute;bottom:0;left:10px;width:20px;height:90px">
+            <div class="background-name background-full text-name">
+                TAVERNE
+            </div>
+        </div>
+        <div style="position:absolute;bottom:0;right:0;height:77%;width:82%">
+            <img class="full-screen" src="images/image_taverne.jpg"/>
+        </div>
+        <div style="position:absolute;bottom:3px;left:50%" class="text-nombreJoueurs">
+            5+
+        </div>
+    </div>
+</div>
+"""
+        config = Config()
+        config.color = "jaune"
+        config.cost = None
+        config.dependency = None
+        config.name = "TAVERNE"
+        config.picture = "image_taverne.jpg"
+        config.players = 5
+        config.effect = {"coin":5}
+        actual = generate_card(config)
+        if not expected == actual:
+            print(actual)
+            assert False
+
+
 
 def main():
     t = CardsGeneratorTester()
