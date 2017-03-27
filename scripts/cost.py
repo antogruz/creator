109e145df46d9c17b31d68d3efcd79142dd39d5a
Antoine Gruzelle <antogruz@gmail.com>
2017-03-27 13:16:20 +0200

    cosmetic

--- scripts/cost.py
+++ scripts/cost.py
@@ -4,13 +4,18 @@ def generate_cost(cost):
     if not cost or len(cost) == 0:
         return ""
 
-    html = wrap_in_div(position(0, 10, 13, 82), '<div class="background-banner background-full"> </div>')
+    html = wrap_in_div(position(0, 10, 13, 82), banner())
     left = 5
     size = 24
     top = 3
     for resource_name in cost:
-        resource = '<img class="full-screen" src="images/{}.png"/>'.format(resource_name)
-        html += wrap_in_div(position(top, left, size, size), resource)
+        html += wrap_in_div(position(top, left, size, size), resource(resource_name))
         top += 25
     return html
+
+def banner():
+    return '<div class="background-banner background-full"> </div>'
+
+def resource(name):
+    return '<img class="full-screen" src="images/{}.png"/>'.format(name)
 
