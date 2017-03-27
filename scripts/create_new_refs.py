05bb6dce7f72f0439a9c12c7b2b39db3b686229e
Antoine Gruzelle <antogruz@gmail.com>
2017-03-27 17:34:35 +0200

    Add references

---
+++ scripts/create_new_refs.py
@@ -0,0 +1,14 @@
+from cards import get_all_cards
+from card_generator import generate_card
+
+def main():
+    for card in get_all_cards():
+        export_ref(card.name, generate_card(card))
+
+def export_ref(filename, card):
+    dir = "scripts/references/"
+    with open(dir + filename, "w") as fh:
+        fh.write(card)
+
+
+main()
