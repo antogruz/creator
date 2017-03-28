8ec551962507905864aec49f2db3fb72714128e4
Antoine Gruzelle <antogruz@gmail.com>
2017-03-28 13:31:39 +0200

    Refactor reference check

--- tests/html_generation.py
+++ tests/html_generation.py
@@ -2,28 +2,37 @@ from unittests import Tester
 from card_generator import generate_card
 import cards
 
-class CardsGeneratorTester(Tester):
-    def aqueduc_test(self):
-        expected = read_file_content("scripts/references/AQUEDUC")
-        actual = generate_card(cards.aqueduc())
-        if not expected == actual:
-            print(actual)
-            assert False
-
-    def taverne_test(self):
-        expected = read_file_content("scripts/references/TAVERNE")
-        actual = generate_card(cards.taverne())
-        if not expected == actual:
-            print(actual)
-            assert False
-
-
 def main():
     t = CardsGeneratorTester()
     t.runTests()
 
+
+class CardsGeneratorTester(Tester):
+    def aqueduc_test(self):
+        confront_reference(cards.aqueduc())
+
+    def taverne_test(self):
+        confront_reference(cards.taverne())
+
+    def caserne_test(self):
+        confront_reference(cards.caserne())
+
+
 def read_file_content(filename):
     with open(filename, "r") as fh:
         return fh.read()
+
+
+def confront_reference(card):
+    expected = read_file_content("scripts/references/{}".format(card.name))
+    actual = generate_card(card)
+    assert_html_equals(expected, actual)
+
+
+def assert_html_equals(expected, actual):
+    if not expected == actual:
+        print (actual)
+        assert False
+
 
 main()
