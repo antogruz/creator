6ba43da9c7876c6ce2e60b8d2c41180c87f8efb2
Antoine Gruzelle <antogruz@gmail.com>
2017-03-28 13:47:15 +0200

    Separate position from size

--- scripts/name.py
+++ scripts/name.py
@@ -1,5 +1,7 @@
 from html import *
 
 def generate_name(card_name):
+    position = ["position:absolute", "bottom:0", "left:10px"]
+    size = ["width:20px", "height:90px"]
     name = wrap_in_div('<div class="background-name background-full text-name">', card_name)
-    return wrap_in_div('<div style="position:absolute;bottom:0;left:10px;width:20px;height:90px">', name)
+    return wrap_in_div(div([style(position + size)]), name)
