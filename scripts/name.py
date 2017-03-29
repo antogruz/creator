815cc3059899968740f8096ae946ff9403d6e691
Antoine Gruzelle <antogruz@gmail.com>
2017-03-29 13:19:40 +0200

    Adjust size of name background with number of letters in name

--- scripts/name.py
+++ scripts/name.py
@@ -2,6 +2,7 @@ import html
 
 def generate_name(card_name):
     position = ["position:absolute", "bottom:0", "left:10px"]
-    size = html.size(20, 90)
+    banner_height = 11 * len(card_name)
+    size = html.size(20, banner_height)
     name = html.wrap_in_div('<div class="background-name background-full text-name">', card_name)
     return html.wrap_in_div(html.div([html.style(position + size)]), name)
