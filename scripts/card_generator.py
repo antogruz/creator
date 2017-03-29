cc24d0cb719b00e769acc2c4acf841fd0312a497
Antoine Gruzelle <antogruz@gmail.com>
2017-03-29 13:55:21 +0200

    Extract effects

--- scripts/card_generator.py
+++ scripts/card_generator.py
@@ -1,6 +1,7 @@
 from cost import generate_cost
 from name import generate_name
 from dependency import generate_dependency
+from effects import generate_effects
 from html import *
 
 def generate_card(config):
@@ -12,40 +13,13 @@ def wrap_in_card_container(content):
 def get_card_content(config):
     content = generate_cost(config.cost)
     content += generate_dependency(config.dependency)
-    content += get_effect(config.effect)
+    content += generate_effects(config.effect)
     content += generate_name(config.name)
     content += get_picture(config.picture)
     content += get_players(config.players)
 
     return wrap('<div class="carte {}">'.format(config.color), content)
 
-
-def get_effect(effects):
-    html = ""
-    for key, value in effects.items():
-        html += create_effect(key, value)
-    return html
-
-def create_effect(label, value):
-    if label == "victory":
-        return victory(value)
-    if label == "coin":
-        return coin(value)
-    if label == "bouclier":
-        return bouclier(value)
-    return ""
-
-def victory(n):
-    content = '<img class="full-screen" src="images/laurier3.png">' + wrap('<div class="center victoire chiffres">', str(n))
-    return wrap(position(10, 90, 60, 55), content)
-
-def coin(n):
-    content = '<img class="full-screen" src="images/coin.png">' + wrap('<div class="center coins chiffres">', str(n))
-    return wrap(position(10, 90, 50, 51), content)
-
-def bouclier(n):
-    content = '<img class="full-screen" src="images/bouclier.png">'
-    return wrap(position(10, 90, 50, 51), content)
 
 def get_picture(file_name):
     return wrap('<div style="position:absolute;bottom:0;right:0;height:77%;width:82%">', '<img class="full-screen" src="images/{}"/>'.format(file_name))
---
+++ scripts/effects.py
@@ -0,0 +1,53 @@
+import html
+
+def generate_effects(effects):
+    representation = ""
+    for key, value in effects.items():
+        representation += create_effect(key, value)
+    return representation
+
+def create_effect(label, value):
+    position = ["position:absolute", "top:10px", "left:90px"]
+    symbol = create_symbol_drawer(label, value)
+    size = symbol.get_dimensions()
+    representation = symbol.get_representation()
+
+    return html.add_style(position + size, representation)
+
+
+def create_symbol_drawer(label, value):
+    if label == "victory":
+        return Victory(value)
+    if label == "coin":
+        return Coins(value)
+    if label == "bouclier":
+        return Bouclier()
+    return None
+
+class Victory:
+    def __init__(self, points):
+        self.points = points
+
+    def get_dimensions(self):
+        return html.size(60, 55)
+
+    def get_representation(self):
+        return '<img class="full-screen" src="images/laurier3.png">' + html.wrap('<div class="center victoire chiffres">', str(self.points))
+
+class Coins:
+    def __init__(self, coins):
+        self.coins = coins
+
+    def get_dimensions(self):
+        return html.size(50, 51)
+
+    def get_representation(self):
+        return '<img class="full-screen" src="images/coin.png">' + html.wrap('<div class="center coins chiffres">', str(self.coins))
+
+
+class Bouclier:
+    def get_dimensions(self):
+        return html.size(50, 51)
+
+    def get_representation(self):
+        return '<img class="full-screen" src="images/bouclier.png">'
