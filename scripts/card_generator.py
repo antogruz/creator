a3544889011d426374ee300397de2e946ed2ef9c
Antoine Gruzelle <antogruz@gmail.com>
2017-03-30 18:48:34 +0200

    Specify what is imported from html

--- scripts/card_generator.py
+++ scripts/card_generator.py
@@ -2,7 +2,7 @@ from cost import generate_cost
 from name import generate_name
 from dependency import generate_dependency
 from effects import generate_effects
-from html import *
+from html import add_style, wrap, div, style, dim, format
 
 def generate_card(config):
     return format(wrap_in_card_container(get_card_content(config)))
@@ -22,8 +22,11 @@ def get_card_content(config):
 
 
 def get_picture(file_name):
-    return wrap('<div style="position:absolute;bottom:0;right:0;height:77%;width:82%">', '<img class="full-screen" src="images/{}"/>'.format(file_name))
+    position = ["position:absolute", "bottom:0", "right:0"]
+    size = ["height:77%", "width:82%"]
+    return add_style(position + size, '<img class="full-screen" src="images/{}"/>'.format(file_name))
 
 def get_players(n):
+    position = ["position:absolute", "bottom:3px", "left:50%"]
     return wrap(div([style(["position:absolute", dim("bottom", 3), dim("left", 50, "%")]), 'class="text-nombreJoueurs"']), "{}+".format(str(n)))
 
