a85a5c8c40b60ede3956bad399cf1db2443dc510
Antoine Gruzelle <antogruz@gmail.com>
2017-03-31 11:03:45 +0200

    list of cards

---
+++ scripts/list_of_cards.py
@@ -0,0 +1,29 @@
+def get_all():
+    cards = ["Académie","Alexandre","Ambassade","Amytis", "Aqueduc","Arène","Archimède","Aristote", "Arsenal" "Aspasie","Atelier","Atelier de siège", "Autel"]
+    cards += ["Bérénice","Bains","Bassin argileux","Bazar", "Bibliothèque","Bilkis","Briqueterie"]
+    cards += ["Cénotaphe","César", "Cabinet d'architecte", "Cabinet d'espions", "Caligula", "Capitole","Caravansérail","Carrière","Caserne","Cavité", "Chambre de commerce", "Chambre des bâtisseurs", "Chambre des confessions", "Champs de tir","Chantier","Cirque","Cléopatre","Comptoir Est","Comptoir Ouest","Confrérie","Consulat","Contingent","Crésus"]
+    cards += ["Darius","Dioclétien","Dispensaire"]
+    cards += ["Ecole","Ecuries","Entrepot secret","Etude","Euclide","Excavation"]
+    cards += ["Filon","Fonderie","Fortifications","Forum","Fosse argileuse","Friche"]
+    cards += ["Gisement", "Guilde des Espions", "Guilde des Joueurs", "Guilde des Ombres"]
+    cards += ["Hôtel de ville", "Hammurabi","Hannibal","Hatshepsout","Hiram","Hypatie"]
+    cards += ["Imhotep"]
+    cards += ["Jardins","Justinien"]
+    cards += ["Léonidas","Laboratoire","Loge"]
+    cards += ["Mécène","Métier à tisser","Maison de jeu","Marché", "Marché noir","Mercenaires","Midas","Milice","Mine","Muraille"]
+    cards += ["Néfertiti","Néron","Nabuchodonosor"]
+    cards += ["Observatoire","Officine"]
+    cards += ["Périclès","Palace","Palissade","Panthéon","Phare","Phidias","Pigeonnier","Place d'armes","Planque","Platon","Port","Portes de la ville","Praxitèle","Presse","Ptolémée","Pythagore"]
+    cards += ["Quai clandestin Est"]
+    cards += ["Réseau clandestin","Résidence","Ramsès","Repaire"]
+    cards += ["Sénat","Sépulcre","Salomon","Sappho","Scierie","Scriptorium","Semiramis","Statue"]
+    cards += ["Tabularium","Taverne","Temple","Tomyris","Tour de garde","Tribunal","Tripot"]
+    cards += ["Université"]
+    cards += ["Varron","Verrerie","Vignoble","Vitruve"]
+    cards += ["Xénophon","Zénobie"]
+
+    return cards
+
+
+if __name__ == "__main__":
+    print(len(get_all()))
