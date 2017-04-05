a4ba56362e122101ecbee8addd66d3c5d2817f42
Antoine Gruzelle <antogruz@gmail.com>
2017-04-05 12:58:59 +0200

    Make destination dir a parameter

--- scripts/download_all.py
+++ scripts/download_all.py
@@ -3,15 +3,27 @@
 
 import urllib
 import list_of_cards
+import os
+import argparse
 
 website = "http://loopinghpf.net/sevenwonders/resources/img/"
 
 def main():
+    destination_dir = parse_destination_dir()
+    ensure_dir_exists(destination_dir)
     for card in list_of_cards.get_all():
-        download(card + ".jpg", "cards")
+        print(card)
+        download(card + ".jpg", destination_dir)
+
+def parse_destination_dir():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("destination_dir")
+    return parser.parse_args().destination_dir
+
+def ensure_dir_exists(d):
+    os.system("mkdir -p {}".format(d))
 
 def download(file, destination):
-    print(file)
     urllib.urlretrieve(website + file, destination + "/" + file)
 
 main()
