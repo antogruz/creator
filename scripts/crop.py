50f84104d3b61657f4e42190105fa1fd24badb8c
Antoine Gruzelle <antogruz@gmail.com>
2017-04-05 13:22:01 +0200

    Script to get all cards pictures

--- scripts/crop.py
+++ scripts/crop.py
@@ -1,17 +1,28 @@
+#!/usr/bin/env python3
+
 import os
 import list_of_cards
+import argparse
 
 def main():
+    (source, destination) = parse_source_and_destination()
+    ensure_dir_exists(destination)
     for card in list_of_cards.get_all():
         print(card)
-        crop_card(card)
+        crop_card(source + "/" + card + ".jpg", destination + "/" + card + ".jpg")
 
-def crop_card(name):
-    x = 107
-    y = 200
-    width = 373
-    height = 560
-    crop("cards/" + name + ".jpg", "view/images/cards_pictures/" + name + ".jpg", x, y, width, height)
+def parse_source_and_destination():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("source")
+    parser.add_argument("destination")
+    args = parser.parse_args()
+    return (args.source, args.destination)
+
+def ensure_dir_exists(d):
+    os.system("mkdir -p {}".format(d))
+
+def crop_card(input, output):
+    crop(input, output, 107, 200, 373, 560)
 
 def crop(src, dest, x, y, width, height):
     src = shellquote(src)
