c3d5d25f0eb027e32709e7153231a3048bcc2cec
Antoine Gruzelle <antogruz@gmail.com>
2017-04-29 19:02:31 +0200

    Elements are centered on the line

--- src/disposition.py
+++ src/disposition.py
@@ -7,7 +7,8 @@ class Line:
         result = ""
         current_left = left
         for e in self.elements:
-            result += e.get(top, current_left)
+            top_position = center(top, top + self.height(), e.height())
+            result += e.get(top_position, current_left)
             current_left += self.padding + e.width()
         return result
 
@@ -16,6 +17,10 @@ class Line:
 
     def width(self):
         return sum([e.width() for e in self.elements]) + sum([self.padding for i in range(1, len(self.elements))])
+
+def center(begin, end, size):
+    empty_space = end - begin - size
+    return empty_space / 2 + begin
 
 class Column:
     def __init__(self, elements, padding):
--- tests/check_disposition.py
+++ tests/check_disposition.py
@@ -17,7 +17,7 @@ class Element:
         return self.h
 
     def get(self, top, left):
-        return "-".join([str(top), str(left)])
+        return "-".join([str(int(top)), str(int(left))])
 
 
 class LineTester(Tester):
@@ -29,14 +29,18 @@ class LineTester(Tester):
         line = Line([Element(0, 0)], 0)
         assert_equals("1-2", line.get(1, 2))
 
-    def test_line_with_several_elements(self):
-        line = Line([Element(2, 3), Element(0, 0)], 5)
+    def test_line_with_several_elements_of_same_height(self):
+        line = Line([Element(2, 3), Element(0, 3)], 5)
         assert_equals("1-0" + "1-7", line.get(1, 0))
 
     def test_line_dimensions(self):
         line = Line([Element(1, 2), Element(3, 1)], 99)
         assert_equals(2, line.height())
         assert_equals(99 + 1 + 3, line.width())
+
+    def test_that_elements_of_different_heights_are_centered(self):
+        line = Line([Element(0, 1), Element(0, 3)], 0)
+        assert_equals("1-0" + "0-0", line.get(0, 0))
 
 
 class ColumnTester(Tester):
