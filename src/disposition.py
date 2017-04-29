d25cbc0accecc37efbb131ea29d8a238b2212a59
Antoine Gruzelle <antogruz@gmail.com>
2017-04-29 18:47:36 +0200

    Line tests and beginning of columns

--- src/disposition.py
+++ src/disposition.py
@@ -10,3 +10,22 @@ class Line:
             result += e.get(top, current_left)
             current_left += self.padding + e.width()
         return result
+
+    def height(self):
+        return max([e.height() for e in self.elements])
+
+    def width(self):
+        return sum([e.width() for e in self.elements]) + sum([self.padding for i in range(1, len(self.elements))])
+
+class Column:
+    def __init__(self, elements, padding):
+        self.elements = elements
+        self.padding = padding
+
+    def get(self, top, left):
+        result = ""
+        current_top = top
+        for e in self.elements:
+            result += e.get(current_top, left)
+            current_top += self.padding + e.height()
+        return result
--- tests/check_disposition.py
+++ tests/check_disposition.py
@@ -1,9 +1,9 @@
 from unittests import Tester, assert_equals
-from disposition import Line
+from disposition import Line, Column
 
 def main():
-    t = LineTester()
-    t.runTests()
+    LineTester().runTests()
+    ColumnTester().runTests()
 
 class Element:
     def __init__(self, w, h):
@@ -30,8 +30,18 @@ class LineTester(Tester):
         assert_equals("1-2", line.get(1, 2))
 
     def test_line_with_several_elements(self):
-        line = Line([Element(2, 2), Element(0, 0)], 5)
+        line = Line([Element(2, 3), Element(0, 0)], 5)
         assert_equals("1-0" + "1-7", line.get(1, 0))
 
+    def test_line_dimensions(self):
+        line = Line([Element(1, 2), Element(3, 1)], 99)
+        assert_equals(2, line.height())
+        assert_equals(99 + 1 + 3, line.width())
+
+
+class ColumnTester(Tester):
+    def test_empty_column(self):
+        col = Column([Element(2, 3), Element(0, 0)], 5)
+        assert_equals("1-0" + "9-0", col.get(1, 0))
 
 main()
