6c02346b40a621770a83de94b93872fc54127daf
Antoine Gruzelle <antogruz@gmail.com>
2017-05-02 20:19:21 +0200

    column and line

--- src/disposition.py
+++ src/disposition.py
@@ -1,36 +1,69 @@
+from enum import Enum
+
+axe_x = 0
+axe_y = 1
+
+def create_line(elements, padding):
+    return Line(Horizontal(), elements, padding)
+
+def create_column(elements, padding):
+    return Line(Vertical(), elements, padding)
+
+class Horizontal():
+    def get_element(self, element, x, y):
+        return element.get(y, x)
+
+    def get_xy(self, top, left):
+        return (left, top)
+
+    def get_size(self, element, axe):
+        if axe == axe_x:
+            return element.width()
+        else:
+            return element.height()
+
+class Vertical():
+    def get_element(self, element, x, y):
+        return element.get(x, y)
+
+    def get_xy(self, top, left):
+        return (top, left)
+
+    def get_size(self, element, axe):
+        if axe == axe_x:
+            return element.height()
+        else:
+            return element.width()
+
 class Line:
-    def __init__(self, elements, padding):
+    def __init__(self, direction, elements, padding):
         self.elements = elements
         self.padding = padding
+        self.direction = direction
 
     def get(self, top, left):
         result = ""
-        current_left = left
+        (x, y) = self.direction.get_xy(top, left)
         for e in self.elements:
-            top_position = center(top, top + self.height(), e.height())
-            result += e.get(top_position, current_left)
-            current_left += self.padding + e.width()
+            e_y = center(y , y + self.size(axe_y), self.direction.get_size(e, axe_y))
+            result += self.direction.get_element(e, x, e_y)
+            x += self.padding + self.direction.get_size(e, axe_x)
         return result
 
-    def height(self):
-        return max([e.height() for e in self.elements])
-
     def width(self):
-        return sum([e.width() for e in self.elements]) + sum([self.padding for i in range(1, len(self.elements))])
+        return self.size(axe_x)
+
+    def height(self):
+        return self.size(axe_y)
+
+    def size(self, axe):
+        if axe == axe_x:
+            return sum([self.direction.get_size(e, axe_x) for e in self.elements]) + sum([self.padding for i in range(1, len(self.elements))])
+        else:
+            return max([self.direction.get_size(e, axe_y) for e in self.elements])
 
 def center(begin, end, size):
     empty_space = end - begin - size
-    return empty_space / 2 + begin
+    result = empty_space / 2 + begin
+    return result
 
-class Column:
-    def __init__(self, elements, padding):
-        self.elements = elements
-        self.padding = padding
-
-    def get(self, top, left):
-        result = ""
-        current_top = top
-        for e in self.elements:
-            result += e.get(current_top, left)
-            current_top += self.padding + e.height()
-        return result
--- tests/check_disposition.py
+++ tests/check_disposition.py
@@ -1,5 +1,5 @@
 from unittests import Tester, assert_equals
-from disposition import Line, Column
+from disposition import create_line, create_column
 
 def main():
     LineTester().runTests()
@@ -22,30 +22,42 @@ class Element:
 
 class LineTester(Tester):
     def test_empty_line(self):
-        line = Line([], 0)
-        assert_equals("", Line([], 0).get(0, 0))
+        line = create_line([], 0)
+        assert_equals("", line.get(0, 0))
 
     def test_line_with_one_element(self):
-        line = Line([Element(0, 0)], 0)
+        line = create_line([Element(0, 0)], 0)
         assert_equals("1-2", line.get(1, 2))
 
     def test_line_with_several_elements_of_same_height(self):
-        line = Line([Element(2, 3), Element(0, 3)], 5)
+        line = create_line([Element(2, 3), Element(0, 3)], 5)
         assert_equals("1-0" + "1-7", line.get(1, 0))
 
     def test_line_dimensions(self):
-        line = Line([Element(1, 2), Element(3, 1)], 99)
+        line = create_line([Element(1, 2), Element(3, 1)], 99)
         assert_equals(2, line.height())
         assert_equals(99 + 1 + 3, line.width())
 
     def test_that_elements_of_different_heights_are_centered(self):
-        line = Line([Element(0, 1), Element(0, 3)], 0)
+        line = create_line([Element(0, 1), Element(0, 3)], 0)
         assert_equals("1-0" + "0-0", line.get(0, 0))
 
 
 class ColumnTester(Tester):
     def test_empty_column(self):
-        col = Column([Element(2, 3), Element(0, 0)], 5)
-        assert_equals("1-0" + "9-0", col.get(1, 0))
+        col = create_column([], 0)
+        assert_equals("", col.get(0, 0))
+
+    def test_column_with_one_element(self):
+        col = create_column([Element(0, 0)], 0)
+        assert_equals("1-2", col.get(1, 2))
+
+    def test_column_with_several_elements_of_same_width(self):
+        col = create_column([Element(3, 2), Element(3, 1)], 5)
+        assert_equals("0-1" + "7-1", col.get(0, 1))
+
+    def test_column(self):
+        col = create_column([Element(3, 4), Element(1, 1)], 5)
+        assert_equals("1-0" + "10-1", col.get(1, 0))
 
 main()
